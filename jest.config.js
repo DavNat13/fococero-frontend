0e4b0b7333d076a97390ad60bad53a09e3bf619f
DavNat13 <natdavn@gmail.com>
2026-04-17 23:02:53 -0400

    chore(core): inicializar arquitectura feature-sliced design y gobernanza

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+﻿module.exports = {
+  preset: 'jest-expo',
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
+  ],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.styles.ts',
+    '!src/**/index.ts'
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 60,
+      branches: 60,
+      functions: 60,
+      lines: 60
+    }
+  }
+};
